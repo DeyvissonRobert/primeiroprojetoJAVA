49eff85c156ac6d2fe7daecaa6298acab9d8ee18
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-26 09:20:49 -0300

    Primeiro desafio: Conta Bncaria

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,4 +1,63 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("""
+                ********************************************
+                Dados iniciais do cliente:
+                
+                Nome:           Kaylanny Silva Costa
+                Tipo conta:     Corrente
+                Saldo inicial:  R$ 2500,00
+                ********************************************
+                """);
+        //        System.out.println("""
+//                ********************************************
+//                Dados iniciais do cliente:
+//                """);
+//        System.out.println("Nome:           Kaylanny Silva Costa");
+//        System.out.println("Tipo conta:     Corrente");
+//        System.out.println("Saldo inicial:  " + saldo);
+//        System.out.println("********************************************");
+        double saldo = 2500.00;
+        int opcoes = 0;
+
+        while (opcoes != 4) {
+            System.out.println("""
+                    
+                    Operações
+                    
+                    1- Consultar saldo
+                    2- Depositar
+                    3- Transferir
+                    4- Sair
+                    """);
+            System.out.println("Digite a opção desejada");
+            opcoes = leitura.nextInt();
+
+            if (opcoes == 1) {
+                System.out.println("O saldo atual é de R$ " + saldo);
+            };
+
+            if (opcoes == 2) {
+                System.out.println("Informe o valor que deseja depositar");
+                saldo += leitura.nextInt();
+                System.out.println("Saldo atualizado R$ " + saldo);
+            };
+
+            if (opcoes == 3) {
+                System.out.println("Digite o valor que deseja transferir");
+
+                if (saldo > leitura.nextInt()) {
+                    saldo -= leitura.nextInt();
+                    System.out.println("Saldo atualizado R$ " + saldo);
+                } else {
+                    System.out.println("Saldo insuficiente");
+                }
+            }
+
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Scream Match!");
+        System.out.println("Filme: Ta chovendo Hamburguer");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        //Média calculada da nota de 3 pessoas
+        double media = (9.7 + 7.4 + 6.5) / 3;
+        System.out.println("média do filme: " + media);
+        String sinopse;
+        sinopse = "Filme de ficção cientifica de um cientista que cria uma máquina que faz chover Hamburguer";
+        System.out.println();
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
     }
 }
+
