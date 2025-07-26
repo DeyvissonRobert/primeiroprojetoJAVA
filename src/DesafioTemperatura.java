93e3600f28c3236194f1bc5d4830e8fa8d7703ce
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-26 09:21:48 -0300

    Desafio: Conversor de temperatura

---
+++ src/DesafioTemperatura.java
@@ -0,0 +1,25 @@
+public class DesafioTemperatura {
+
+    public static void main(String[] args) {
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+        String nome = "João";
+        int aulas = 4;
+
+        String mensagemd = """
+                  Olá, %s!
+                  Boas vindas ao curso de Java.
+                  Teremos %d aulas para te mostrar o que é preciso para você dar o seu primeiro mergulho na linguagem!
+                  """.formatted(nome, aulas);
+
+        System.out.println(mensagemd);
+    }
+
+}
